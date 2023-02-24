86a730cf59b5f806dd047521ba21df2c22e763c2
csr <camilaseemannramos@gmail.com>
2023-02-24 09:39:45 +0100

    Footer error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Info from "../components/Info";
 import About from "../components/About";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import Interests from "../components/Interests";
 
 export default function App() {
